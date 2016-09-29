33bf24c6386d1641080e118fa1265fdf22da35d8
Tanin Rojanapiansatith <sp_land@hotmail.com>
2016-09-29 15:59:16 +0100

    Initial Commit

---
+++ ArmyStrength(Easy)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ArmyStrength(Easy)
+//
+//  Created by landtanin on 9/29/2559 BE.
+//  Copyright © 2559 landtanin. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
